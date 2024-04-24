e108493e913ffe0c85619daac75f0e8619dbec65
sia miniyar <141447423+siaa1308@users.noreply.github.com>
2024-04-24 22:04:56 +0530

    Update grocery.h

--- grocery/grocery.h
+++ grocery/grocery.h
@@ -1,9 +1,19 @@
 #ifndef GROCERY_H_
 #define GROCERY_H_
 
-float calculatePrice(int qty, float price);
-void displayBill(int apple_qty, int banana_qty, int orange_qty, int mango_qty, int pear_qty);
-void viewItemsInCart(int apple_qty, int banana_qty, int orange_qty, int mango_qty, int pear_qty);
-void viewPrices();
+typedef struct {
+    char name[50];  
+    float price;    
+    int quantity;   
+} Item;
 
-#endif /* GROCERY_H_ */
+float calculatePrice(int qty, float price);
+void displayBill(Item *cart, int numItems);
+void viewItemsInCart(Item *cart, int numItems);
+void viewPrices(Item *items, int numItems);
+Item *addItemToCart(Item *cart, int numItems, Item *items, const char *itemName, int quantity);
+Item *createItems();
+void freeItems(Item *items);
+Item *findItemByName(Item *items, int numItems, const char *itemName);
+
+#endif
