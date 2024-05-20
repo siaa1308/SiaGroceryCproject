24a33f439e5b4e666eff35d7c1015d8640159252
sia miniyar <141447423+siaa1308@users.noreply.github.com>
2024-05-20 13:33:18 +0530

    Update and rename viewItemsInCart.c to fileOperations.h

---
+++ grocery/fileOperations.h
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fileOperations.h"
+#include "itemOperations.h"
+
+#define ITEM_FILE "items.csv"
+
+void loadItemsFromFile(struct Item **items, int *itemCount) {
+    FILE *file = fopen(ITEM_FILE, "r");
+    if (file == NULL) {
+        printf("Error opening items file.\n");
+        exit(1);
+    }
+
+    *itemCount = 0;
+    char ch;
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == '\n') {
+            (*itemCount)++;
+        }
+    }
+    rewind(file);
+
+    *items = (struct Item *)malloc(*itemCount * sizeof(struct Item));
+    if (*items == NULL) {
+        printf("Memory allocation error.\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < *itemCount; i++) {
+        fscanf(file, "%[^,],%f\n", (*items)[i].name, &(*items)[i].price);
+    }
+
+    fclose(file);
+}
+
+void loadCustomersFromFile() {
+    FILE *file = fopen(CUSTOMER_FILE, "r");
+    if (file == NULL) {
+        printf("Error opening customers file!\n");
+        exit(1);
+    }
+
+    char line[100];
+    printf("Customer IDs and Names:\n");
+    while (fgets(line, sizeof(line), file)) {
+        printf("%s", line);
+    }
+
+    fclose(file);
+}
